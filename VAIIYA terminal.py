79693ad4435c6f1df938700e725ba92f28f39894
T342 <147361653+T342guy@users.noreply.github.com>
2024-11-16 21:07:17 -0800

    fixed a HELL ton of typos LMAO

--- VAIIYA terminal.py
+++ VAIIYA terminal.py
@@ -13,14 +13,14 @@ from datetime import datetime
 from prompt_toolkit import print_formatted_text, HTML
 import random
 import tkinter.messagebox 
-#changes the size of the Command promp so it is easyer to read (and that the ASCII doesnt soft wrap)
+#changes the size of the Command prompt so it is easier to read (and that the ASCII doesnt soft wrap)
 
 def check_for_update_plz():
     tkinter.messagebox.showinfo(title='Check for updates :3', message='hey! check to see if you are using the latest version! :3')
     
 
 #this idea was requested by Smashel on issue #30. 
-#defualt value is 1-5650
+#default value is 1-5650
 #if it somehow gets that number the ascii will "corrupt" and use an alt
 def startup_screen_ascii_roll():
     corrupted_ascii_roll = random.randint(1,5650)
@@ -91,8 +91,8 @@ def norm_startup_screen_ASCII():
                                             
                                     please wait while the program does mandatory checks.
     """)
-#title stuff for new loadin screen 
-#BEHAVEIOR NOTE: THE PROMPT TK LOADINGBARS CANNOT GO OVER .01 FLOAT FOR SOME REASON! (thanks smashel for the idea, but was unable to make it happen)
+#title stuff for new loading screen 
+#BEHAVIOR NOTE: THE PROMPT TK LOADINGBARS CANNOT GO OVER .01 FLOAT FOR SOME REASON! (thanks smashel for the idea, but was unable to make it happen)
 def loading_bars_intro_1():
 
     title = HTML('Connecting to the VAIIYA Defender framework....')
@@ -192,11 +192,11 @@ def message_of_the_day(): #or per boot
     MOTD = random.randint(1,3)
 
     if MOTD == 1: 
-        print("john? what the hell are you doing over there? GET BACK TO WO- ##TRANSSCRIPT ENDED CODE 87##")
+        print("john? what the hell are you doing over there? GET BACK TO WO- ##TRANSCRIPT ENDED CODE 87##")
     elif MOTD == 2: 
         print("Remember: CNS is our greatest enemy! WE CANNOT LET THEM INTO THE SYSTE- ## LOG SYSTEM FAILURE CODE #9)*^9 ##")
     elif MOTD == 3:
-        print("SYSTEM AUTOLOG OVRRIDE: SERVERS #108,#196,#102,#156,#342 HAVE BEEN ISOLATED. DO NOT CHANGE THIS ORDER.")
+        print("SYSTEM AUTOLOG OVERRIDE: SERVERS #108,#196,#102,#156,#342 HAVE BEEN ISOLATED. DO NOT CHANGE THIS ORDER.")
 
 def timefetch():
 #time fetch for login
@@ -214,7 +214,7 @@ def terminal_start_message():
     print(" for a list of commands, please type 'commands' ")
     print("""|""")
 
-#TERMINAL BEHAVIOR NOTES! make sure to use `elif` instead of `if`. this will prevent the error string from printing if we retun from the EEs or logins.
+#TERMINAL BEHAVIOR NOTES! make sure to use `elif` instead of `if`. this will prevent the error string from printing if we return from the EEs or logins.
 #ANOTHER NOTE: exit() AND quit() COUNT AS DEBUGGING, SO A TRACKBACK WILL CALL. USE `raise SystemExit` FROM NOW ON!
 #
 #notes here^^^
@@ -229,7 +229,7 @@ def open_terminal():
 #put all the usercommands under here please! 
         #this is the FIRST `if`, replace and all new should be `elif`.
         if text == 't342':
-            print('heyy thanks for sayin somthin!')
+            print('hey thanks for saying something!')
             continue
 #this is BELOW the first command. put `elif` on all new commands.
 
@@ -254,7 +254,7 @@ def open_terminal():
             print("VAIIYA Terminal Engine version 23.58-B ")
             print("detected operating device: IBM 5150 VAIIYA secure system fitted.")
             print("OS system detected: MuCoDOS V27.592 private")
-            print("VAIIYA Terminal realease V10-PRE")
+            print("VAIIYA Terminal release V10-PRE")
             print("|")
             #the link given will NEVER EXPIRE
         elif text == 'discord':
@@ -265,7 +265,7 @@ def open_terminal():
 
 
         elif text == 'CNS':
-            print("CNS_VAIIYA_BYPASS_V4.567.EXE EXITCUTING....")
+            print("CNS_VAIIYA_BYPASS_V4.567.EXE EXECUTING....")
             time.sleep(2)
             CNS_EE_HAKED()
         
@@ -298,7 +298,7 @@ def open_terminal():
         elif text == 'commands':
             print("""|""")
             print("""|""")
-            print("Avalible commands: (all may not be listed.)")
+            print("Available commands: (all may not be listed.)")
             print("""|""")
             print("command; walker | The login for CM|walker")
             print("command; frostbyte | The login for CM|frostbyte")
@@ -357,7 +357,7 @@ def DEBUG_CMD():
 
 
 #PUT ALL FUNC DEFS BELOW HERE! 
-# the CNS EE below this messange
+# the CNS EE below this message
 def CNS_EE_HAKED():
     #below is the Y/N prompt for CNS, and the following `result` can be split into a bool and set as True or False
     result = yes_no_dialog(
@@ -368,7 +368,7 @@ def CNS_EE_HAKED():
          message_dialog(
     title='CNS.02.06.01',
     text='very well then, we will see you soon enough').run()
-         #then after it retuns to the main menu and exits the program.
+         #then after it returns to the main menu and exits the program.
          print("VAIIYA DEFENDER ENGINE CRITICAL FAILURE!: THE VAIIYA DEFENDER ENGINE HAS FOUND A BREACH AND WILL NOW FORCE QUIT THE PROGRAM")
          print("""|""")
          print("THE PROGRAM WILL SHUTDOWN IN:")
@@ -384,7 +384,7 @@ def CNS_EE_HAKED():
     if result == False:
          message_dialog(
     title='CNS.02.06.01',
-    text='how dissapointing, that you dont want tHe TrutH. we will see you soon enough').run()
+    text='how disappointing, that you dont want tHe TrutH. we will see you soon enough').run()
          print("VAIIYA DEFENDER ENGINE CRITICAL FAILURE!: THE VAIIYA DEFENDER ENGINE HAS FOUND A BREACH AND WILL NOW FORCE QUIT THE PROGRAM")
          print("""|""")
          print("THE PROGRAM WILL SHUTDOWN IN:")
@@ -400,7 +400,7 @@ def CNS_EE_HAKED():
 
 
 
-#add passwords here for the logins and name the vars respectivly.
+#add passwords here for the logins and name the vars respectively.
 # 
 #the website for reference to the password system is https://www.geeksforgeeks.org/npm-bcrypt/   
 #walkerpasswrd1
@@ -411,24 +411,24 @@ frosthash = b'$2b$12$AUur7AKX1aGQurOlmM46Pu0OX9HXqx6UHH9SHiEvrCJM56JvUjYfu'
 
 #walker login here
 def walker_login():
-    #when exiting the prompt with the `<cancel>`, the program will forcequit for some reason. 
+    #when exiting the prompt with the `<cancel>`, the program will force-quit for some reason. 
 
     #password prompt; 
     userpassword = text = input_dialog(
     title='Walker password input',
     text='walker password:').run()
-    #encodes the given password for comapare
+    #encodes the given password for compare
     userpassword = userpassword.encode('utf-8')
 
-    #comapre password hashes, if identical then "result" == True, then it will move onto walker_entered
+    #compare password hashes, if identical then "result" == True, then it will move onto walker_entered
     result = bcrypt.checkpw(userpassword, walkerhash)
     if result:
           walker_entered()
-#end of walker password varifi 
+#end of walker password verify 
 
 
 def walker_entered():
-    print("welcome walker! here currnently there is nothing, i have no idea what to put here for you guys.")
+    print("welcome walker! here currently there is nothing, i have no idea what to put here for you guys.")
     print("but id assume you are familiar with github so if you have an idea i would more than glad take a look and try to implement it! ")
     text = prompt("type EXIT to exit this page; ")
     if text == 'exit':
@@ -440,7 +440,7 @@ def walker_entered():
 # FROST EE STUFF OVER HERE!
 def frostbyte_login():
                 
-    #there is a bug that cuases the `no command` string to print when exiting.
+    #there is a bug that causes the `no command` string to print when exiting.
     print("to exit, type EXIT in the password!")
     userpassword = text = input_dialog(
     title='frostbyte password input',
@@ -448,7 +448,7 @@ def frostbyte_login():
     
     userpassword = userpassword.encode('utf-8')
                
-    #comapre password hashes
+    #compare password hashes
     result = bcrypt.checkpw(userpassword, frosthash)
     if result:
           frostbytes_EE_entered()
@@ -460,7 +460,7 @@ def frostbyte_login():
 # 2nd part to the FROST EE                     
 def frostbytes_EE_entered():
     
-    #the following prompts from promptTK are for frost taling about UwU more and more ¯\_(ツ)_/¯
+    #the following prompts from promptTK are for frost taking about UwU more and more ¯\_(ツ)_/¯
     message_dialog(
     title='VAIIYA Warning systems',
     text='VAIIYA TERMINAL WARNING AWAITING ATTENTION!').run()
@@ -473,24 +473,35 @@ def frostbytes_EE_entered():
     title='VAIIYA Warning systems',
     text='thank you for your attention. you may continue your tasks and have a safe day!').run()
     
+    print("|")
+    print("|")
+    print("use 'commands' for available") 
+    
     while True:
-        prompt('system$frostbyte>>> ')
+        text = prompt('system$frostbyte>>> ')
+
+        if text == 'commands':
+            print("commands for sys$frostbyte ")
+            print("command; VRRALSA")
+
+        elif text == 'VRRALSA':
+            VRRALSA_startup()
     
 #END OF FROST EE CODE, 
 
 
 #USER TEMPLATE HERE
             #print("|")
-            #print("VAIIYA RESTRICTED COMMAND LINE (V.R.C.L.) FINGING INQUIRY....")
+            #print("V.R.R.A.L.S.A. FINDING INQUIRY....")
             #time.sleep(1)
-            #print("serching database... ")
+            #print("searching database... ")
             #time.sleep(1)
             #print("Record found!")
             #print("|")
             #print("citizen record; (name) ")
             #print("username=(DC username)")
             #print("user_traits= (any) ")
-            #print("health_record_status=(True/false) current_records=## record_severety=(low/med/high) highest_alert=(any)")
+            #print("health_record_status=(True/false) current_records=## record_severity=(low/med/high) highest_alert=(any)")
             #print("end of file.")
             #print("|")
 
@@ -518,8 +529,8 @@ def VRRALSA_welcome_message():
     print("Welcome VAIIYA trustee or other high authorization. ")
     print("|")
     print("This is the V.AIIYA R.ESTRICTED R.ECORDS A.ND L.OGS S.YSTEM A.UTOMATED Terminal.")
-    print("where some (not all) user records are found. use keywords to acsess records.")
-    print("use command 'records' for avalable records ")
+    print("where some (not all) user records are found. use keywords to access records.")
+    print("use command 'records' for available records ")
 def VRRALSA_COMMAND_PANEL():
     
     while True:
@@ -527,7 +538,7 @@ def VRRALSA_COMMAND_PANEL():
         VRRALSA_TEXT = prompt('V.R.R.A.L.S.A. awaiting command(s)>>>> ')
 
         if  VRRALSA_TEXT == 'test':
-            print("yes I do beleve it works! ")
+            print("yes I do believe it works! ")
         
 
         elif VRRALSA_TEXT == 'records':
@@ -538,24 +549,24 @@ def VRRALSA_COMMAND_PANEL():
 
         elif VRRALSA_TEXT == 'violet':
             print("|")
-            print("V.R.R.A.L.S.A. FINGING INQUIRY....")
+            print("V.R.R.A.L.S.A. FINDING INQUIRY....")
             time.sleep(1)
-            print("serching database... ")
+            print("searching database... ")
             time.sleep(1)
             print("Record found!")
             print("|")
             print("citizen record; violet ")
             print("username=violet_120")
-            print("user_traits= dormant and non-reposnsive when on duty. does not interact without a ping. ")
-            print("health_record_status=True current_records=7 record_severety=low highest_alert=NONE")
+            print("user_traits= dormant and non-responsive when on duty. does not interact without a ping. ")
+            print("health_record_status=True current_records=7 record_severity=low highest_alert=NONE")
             print("end of file.")
             print("|")
 
         elif VRRALSA_TEXT == 'frostbyte':
             print("|")
-            print("V.R.R.A.L.S.A. FINGING INQUIRY....")
+            print("V.R.R.A.L.S.A. FINDING INQUIRY....")
             time.sleep(1)
-            print("serching database... ")
+            print("searching database... ")
             time.sleep(1)
             print("Record found!")
             print("|")
@@ -563,22 +574,22 @@ def VRRALSA_COMMAND_PANEL():
             print("username=frost.dime")
             print("user_traits= funny, quote'says UwU OwO and other. please advise' ")
             print("health_record_status=True current_health_records=1479 current_untracked_health_records=1457 ")
-            print("record_severety=high highest_alert=severe mental issues unreported. ")
+            print("record_severity=high highest_alert=severe mental issues unreported. ")
             print("end of file.")
             print("|")
 
         elif VRRALSA_TEXT == 'walker':
             print("|")
-            print("V.R.R.A.L.S.A. FINGING INQUIRY....")
+            print("V.R.R.A.L.S.A. FINDING INQUIRY....")
             time.sleep(1)
-            print("serching database... ")
+            print("searching database... ")
             time.sleep(1)
             print("Record found!")
             print("|")
-            print("citizen record; Walker  user_traits=enjoys_'dinos' drinks_accsessive_amounts_of_coffee ")
+            print("citizen record; Walker  user_traits=enjoys_'dinos' drinks_excessive_amounts_of_coffee ")
             print("username=walkercm")
-            print("user_traits=enjoys_'dinos' drinks_accsessive_amounts_of_coffee")
-            print("health_record_status=True current_records=17 record_severety=low highest_alert=heart_attack_of_coffee")
+            print("user_traits=enjoys_'dinos' drinks_excessive_amounts_of_coffee")
+            print("health_record_status=True current_records=17 record_severity=low highest_alert=heart_attack_of_coffee")
             print("end of file.")
             print("|")
 
